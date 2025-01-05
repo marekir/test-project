f701766791330cbec4d3bf1d6c7a8ec6a423c58b
Rita <mardekrim@gmail.com>
2025-01-05 23:45:56 +0300

    changes for my-local-branch

--- test_list3.py
+++ test_list3.py
@@ -1,11 +1,4 @@
-'''lst=[int(i) for i in input().split()]
-n=int(input())
-if lst.count(n)==0:
-    print('Отсутствует')
-else:
-    for i in range(0,len(lst)):
-        if lst[i]==n:
-            print(i, end=' ')'''
+'''changes for my-local-branch'''
 a=[]
 b=[]
 while 1:
